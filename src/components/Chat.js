169be5234aec5dea85055710ec801acb0ed6bda1
berkayakin <siratofficial@gmail.com>
2021-02-05 10:59:57 +0100

    Removed unnecessary console.log

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -100,8 +100,6 @@ export default function Chat(props) {
     }, [waId]);
 
     useEffect(() => {
-        console.warn(source);
-
         // Consider replacing this with IntersectionObserver
         // Browser support should be considered: https://caniuse.com/intersectionobserver
         function handleScroll(e) {
