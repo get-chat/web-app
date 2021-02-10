ec58fe728da9a53fbc7d21c8d1f81743ca9f448d
berkayakin <siratofficial@gmail.com>
2021-02-10 13:32:46 +0100

    Search message feature update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,16 @@ import {SearchOutlined} from "@material-ui/icons";
 import '../styles/SearchBar.css';
 
 function SearchBar(props) {
+
+    const handleChange = (event) => {
+        props.onChange(event.target.value);
+    }
+
     return (
         <div className="searchBar__search">
             <div className="searchBar__searchContainer">
                 <SearchOutlined />
-                <input placeholder="Search" type="text" />
+                <input placeholder="Search" type="text" onChange={handleChange} />
             </div>
         </div>
     )
--- src/components/SearchMessage.js
+++ src/components/SearchMessage.js
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../styles/SearchMessage.css';
 import {IconButton} from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 import PubSub from "pubsub-js";
-import {EVENT_TOPIC_SEARCH_MESSAGES_VISIBILITY} from "../Constants";
+import {BASE_URL, EVENT_TOPIC_SEARCH_MESSAGES_VISIBILITY} from "../Constants";
 import SearchBar from "./SearchBar";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {getConfig} from "../Helpers";
+import ChatMessageClass from "../ChatMessageClass";
 
 function SearchMessage(props) {
 
+    const [keyword, setKeyword] = useState();
+    const {waId} = useParams();
+
+    let cancelToken;
+    let source;
+
+    const generateCancelToken = () => {
+        cancelToken = axios.CancelToken;
+        source = cancelToken.source();
+    }
+
+    // Generating cancel token
+    generateCancelToken();
+
     const hideSearchMessages = () => {
         PubSub.publish(EVENT_TOPIC_SEARCH_MESSAGES_VISIBILITY, false);
+    }
+
+    useEffect(() => {
+        // Cancelling previous request
+        // TODO: Add a promise to continue with next request
+        source.cancel();
+        if (keyword && keyword.trim().length > 0) {
+            search(keyword.trim());
+        }
+    }, [keyword]);
+
+    const search = (_keyword) => {
+        console.log(_keyword);
+
+        axios.get( `${BASE_URL}messages/${waId}/`,
+            getConfig({
+                //offset: offset ?? 0,
+                limit: 30,
+                keyword: _keyword
+            }, source.token)
+        )
+            .then((response) => {
+                console.log("Messages", response.data);
+
+                const preparedMessages = {};
+                response.data.results.reverse().map((message, index) => {
+                    const prepared = new ChatMessageClass(message);
+                    preparedMessages[prepared.id] = prepared;
+                });
+
+            })
+            .catch((error) => {
+                // TODO: Handle errors
+
+                //displayError(error);
+            });
     }
 
     return (
@@ -22,7 +76,7 @@ function SearchMessage(props) {
                 <h3>Search For Messages</h3>
             </div>
 
-            <SearchBar />
+            <SearchBar onChange={setKeyword} />
 
             <div className="searchMessage__body">
 
