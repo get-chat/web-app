aba19c6dcae275ddae30cb4e67f098eefa60fe31
berkayakin <siratofficial@gmail.com>
2021-03-03 13:10:41 +0100

    Reply message feature styles update

--- src/components/ContextChatMessage.js
+++ src/components/ContextChatMessage.js
@@ -7,14 +7,15 @@ function ContextChatMessage(props) {
     return (
         <div className="chat__message__context" onClick={() => props.goToMessageId(props.contextMessage.id, props.contextMessage.timestamp)}>
             <div className="chat__message__context__info">
-                        <span className="chat__message__context__info__sender">
-                            {props.contextMessage.senderName}
-                        </span>
+                <span className="chat__message__context__info__sender">
+                    {props.contextMessage.senderName}
+                </span>
+
                 <span className="chat__message__context__info__message">
-                            <ChatMessageTypeIcon type={props.contextMessage.type} />
+                    <ChatMessageTypeIcon type={props.contextMessage.type} />
                     {props.contextMessage.text}
                     {props.contextMessage.caption}
-                        </span>
+                </span>
             </div>
 
             {props.contextMessage.type === ATTACHMENT_TYPE_IMAGE &&
