39f1d468702163ac02bf3898946ac588c13c20c7
berkayakin <siratofficial@gmail.com>
2021-03-12 11:28:22 +0100

    Mobile version update

--- src/components/SearchMessage.js
+++ src/components/SearchMessage.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import {getConfig} from "../Helpers";
 import ChatMessageClass from "../ChatMessageClass";
 import SearchMessageResult from "./SearchMessageResult";
-import {isMobile} from 'react-device-detect';
+import {isMobileOnly} from 'react-device-detect';
 
 function SearchMessage(props) {
 
@@ -74,7 +74,7 @@ function SearchMessage(props) {
     const goToMessage = (data) => {
         PubSub.publish(EVENT_TOPIC_GO_TO_MSG_ID, data);
 
-        if (isMobile) {
+        if (isMobileOnly) {
             hideSearchMessages();
         }
     }
