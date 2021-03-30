6a15882202fffe08aefe14e98ae917b53ea56b44
berkayakin <siratofficial@gmail.com>
2021-03-30 12:23:33 +0200

    Phone number links update

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -53,7 +53,7 @@ function ContactDetails(props)  {
 
                 <div className="contactDetails__body__section">
                     <div className="contactDetails__body__section__title">Whatsapp Phone Number</div>
-                    <span>{'+' + props.contactData.waId}</span>
+                    <a href={"tel:+" + props.contactData.waId}>{'+' + props.contactData.waId}</a>
                 </div>
 
                 {props.contactProvidersData[props.contactData.waId]?.map((providerData, index) =>
