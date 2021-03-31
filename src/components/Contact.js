598401e099edc2aec34513bc694b8029096ffdee
berkayakin <siratofficial@gmail.com>
2021-03-31 10:58:38 +0200

    Contacts update

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Avatar} from "@material-ui/core";
+
+function Contact(props) {
+    return (
+        <div>
+            <Avatar />
+        </div>
+    )
+}
+
+export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -6,10 +6,12 @@ import {getConfig} from "../Helpers";
 import SearchBar from "./SearchBar";
 import {IconButton} from "@material-ui/core";
 import {ArrowBack} from "@material-ui/icons";
+import Contact from "./Contact";
 
 function Contacts(props) {
 
     const [keyword, setKeyword] = useState("");
+    const [contacts, setContacts] = useState({});
 
     let cancelTokenSourceRef = useRef();
 
@@ -24,12 +26,25 @@ function Contacts(props) {
         }
     }, []);
 
+    useEffect(() => {
+        // Generate a token
+        cancelTokenSourceRef.current = axios.CancelToken.source();
+
+        findContacts();
+
+        return () => {
+            cancelTokenSourceRef.current.cancel();
+        }
+    }, [keyword]);
+
     const findContacts = () => {
         axios.get(`${BASE_URL}contacts/`, getConfig({
             search: keyword
         }, cancelTokenSourceRef.current.token))
             .then((response) => {
                 console.log("Contacts list", response.data);
+
+                setContacts(response.data);
 
             })
             .catch((error) => {
@@ -51,6 +66,12 @@ function Contacts(props) {
 
             <SearchBar
                 onChange={setKeyword} />
+
+            <div className="contacts__body">
+                { Object.entries(contacts).map((message) =>
+                    <Contact />
+                )}
+            </div>
 
         </div>
     )
