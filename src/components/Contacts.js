577ad3089d16d054ab6fb339eaff18ee73c73b84
berkayakin <siratofficial@gmail.com>
2021-03-31 09:31:47 +0200

    Contacts initial

---
+++ src/components/Contacts.js
@@ -0,0 +1,20 @@
+import React, {useEffect} from "react";
+
+function Contacts(props) {
+
+    useEffect(() => {
+        findContacts();
+    }, []);
+
+    const findContacts = () => {
+
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Contacts;
