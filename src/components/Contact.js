e102801d940ab929ad910247a956267559ecf975
berkayakin <siratofficial@gmail.com>
2021-03-31 15:22:13 +0200

    Contacts update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/Contact.css';
 import {Avatar, ListItem} from "@material-ui/core";
 import ContactProviderHeader from "./ContactProviderHeader";
 
 function Contact(props) {
+
+    const [phoneNumbersVisible, setPhoneNumbersVisible] = useState(false);
+
+    const handleClick = () => {
+        if (props.data.phoneNumbers && props.data.phoneNumbers.length > 0) {
+            if (props.data.phoneNumbers.length === 1) {
+                const phoneNumber = props.data.phoneNumbers[0].phone_number;
+            } else {
+                setPhoneNumbersVisible(prevState => !prevState);
+            }
+        }
+    }
+
     return (
         <div className="contactWrapper">
             <ListItem button>
-                <div className="contact">
+                <div className="contact" onClick={handleClick}>
                     <div className="contact__avatarWrapper">
                         <Avatar src={props.data.avatar}>{props.data.initials}</Avatar>
                         <ContactProviderHeader type={props.data.contactProvider} />
@@ -23,12 +36,23 @@ function Contact(props) {
 
                         {(props.data.phoneNumbers && props.data.phoneNumbers.length > 1) &&
                         <div className="contact__info__otherPhoneNumbers">
-                            <span>{props.data.phoneNumbers?.length - 1} more phone numbers</span>
+                            <span>{props.data.phoneNumbers?.length - 1} more phone number(s)</span>
                         </div>
                         }
                     </div>
                 </div>
             </ListItem>
+
+            {phoneNumbersVisible &&
+            <div className="contactPhoneNumbersChoices">
+                <h3>Choose a phone number</h3>
+                { Object.entries(props.data.phoneNumbers).map((phoneNumber) =>
+                    <div key={phoneNumber[0]} className="contactPhoneNumbersChoices__choice">
+                        {phoneNumber[1].phone_number}
+                    </div>
+                )}
+            </div>
+            }
         </div>
     )
 }
