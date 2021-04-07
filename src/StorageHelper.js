0a77ad437b026e890d904b5ba61da1baa269913f
berkayakin <siratofficial@gmail.com>
2021-04-07 09:50:36 +0200

    Storage helper update

--- src/StorageHelper.js
+++ src/StorageHelper.js
@@ -1,20 +1,39 @@
 const STORAGE_TAG_TOKEN = "token";
+const STORAGE_TAG_CONTACT_PROVIDERS_DATA = "contact_providers_data"
+const STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME = "contact_providers_data_time"
+
 export const getToken = () => {
     return localStorage.getItem(STORAGE_TAG_TOKEN);
 }
+
 export const storeToken = (token) => {
     localStorage.setItem(STORAGE_TAG_TOKEN, token);
 }
+
 export const clearToken = () => {
     localStorage.removeItem(STORAGE_TAG_TOKEN);
 }
-const STORAGE_TAG_CONTACT_PROVIDERS_DATA = "contact_providers_data"
+
 export const getContactProvidersData = () => {
     return JSON.parse(localStorage.getItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA)) ?? {};
 }
+
 export const storeContactProvidersData = (data) => {
     localStorage.setItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA, JSON.stringify(data));
 }
+
 export const clearContactProvidersData = () => {
     localStorage.removeItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA);
+}
+
+export const getContactProvidersDataTime = () => {
+    return JSON.parse(localStorage.getItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME));
+}
+
+export const storeContactProvidersDataTime = (time) => {
+    localStorage.setItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME, time);
+}
+
+export const clearContactProvidersDataTime = () => {
+    localStorage.removeItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME);
 }
