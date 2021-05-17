726cd9bd1068bdacb1c179cd8268e5b7cd39b112
berkayakin <siratofficial@gmail.com>
2021-05-17 10:00:08 +0200

    Chat tag list component update

--- src/components/ChatTagsList.js
+++ src/components/ChatTagsList.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {BASE_URL} from "../Constants";
+import {getConfig} from "../Helpers";
 
 function ChatTagsList(props) {
+
+    const [isLoading, setLoading] = useState(true);
+    const [tags, setTags] = useState([]);
+
+    useEffect(() => {
+        listTags();
+    }, []);
+
+    const listTags = () => {
+        axios.get( `${BASE_URL}tags/`, getConfig())
+            .then((response) => {
+                console.log("Tags: ", response.data);
+
+                setTags(response.data.results);
+
+                setLoading(false);
+            })
+            .catch((error) => {
+                window.displayError(error);
+            });
+    }
+
     return (
         <div>
-
+            {tags &&
+            <div>
+                {tags.map((tag) =>
+                <div>
+                    {tag.name}
+                </div>
+                )}
+            </div>
+            }
         </div>
     )
 }
