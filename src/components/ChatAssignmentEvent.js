ea40f61b5a525dc7aa0227ee97a9001c36331041
berkayakin <siratofficial@gmail.com>
2021-05-18 14:10:00 +0200

    Assignment and tag histories update

--- src/components/ChatAssignmentEvent.js
+++ src/components/ChatAssignmentEvent.js
@@ -5,16 +5,34 @@ function ChatAssignmentEvent(props) {
     return (
         <div className="chatAssignmentEvent">
             <div className="chatAssignmentEvent__content">
-                <span className="bold">{props.data.done_by?.username}</span>:
+                <div className="chatAssignmentEvent__content__title">
+                    <span className="bold">{props.data.done_by?.username}</span> has changed chat assignments.
+                </div>
                 {/*{JSON.stringify(props.data)}*/}
 
-                {props.data.assigned_to_user_set ? ' assigned to user: ' + props.data.assigned_to_user_set.username : ''}
+                {props.data.assigned_to_user_set &&
+                <div>
+                    assigned to user: {props.data.assigned_to_user_set.username}
+                </div>
+                }
 
-                {props.data.assigned_group_set ? ' assigned to group: ' + props.data.assigned_group_set.name : ''}
+                {props.data.assigned_group_set &&
+                <div>
+                    assigned to group: {props.data.assigned_group_set.name}
+                </div>
+                }
 
-                {props.data.assigned_to_user_was_cleared ? ' cleared assigned user' : ''}
+                {props.data.assigned_to_user_was_cleared &&
+                <div>
+                    cleared assigned user
+                </div>
+                }
 
-                {props.data.assigned_group_was_cleared ? ' cleared assigned group' : ''}
+                {props.data.assigned_group_was_cleared &&
+                <div>
+                    cleared assigned group
+                </div>
+                }
             </div>
         </div>
     )
