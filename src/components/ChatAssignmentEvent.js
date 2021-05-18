4552753c3cd1b7982a3cb881ccdbe905b8eb4a41
berkayakin <siratofficial@gmail.com>
2021-05-18 12:41:53 +0200

    Assignment and tag histories update

--- src/components/ChatAssignmentEvent.js
+++ src/components/ChatAssignmentEvent.js
@@ -1,16 +1,21 @@
 import React from "react";
+import '../styles/ChatAssignmentEvent.css';
 
 function ChatAssignmentEvent(props) {
     return (
-        <div>
-            {props.data.done_by?.username}
-            {/*{JSON.stringify(props.data)}*/}
+        <div className="chatAssignmentEvent">
+            <div className="chatAssignmentEvent__content">
+                <span className="bold">{props.data.done_by?.username}</span>:
+                {/*{JSON.stringify(props.data)}*/}
 
-            {props.data.assigned_to_user_set ? ' assigned to user: ' + props.data.assigned_to_user_set.username : ''}
+                {props.data.assigned_to_user_set ? ' assigned to user: ' + props.data.assigned_to_user_set.username : ''}
 
-            {props.data.assigned_group_set ? ' assigned to group: ' + props.data.assigned_group_set.name : ''}
+                {props.data.assigned_group_set ? ' assigned to group: ' + props.data.assigned_group_set.name : ''}
 
-            {props.data.assigned_to_user_was_cleared ? ' cleared assigned user' : ''}
+                {props.data.assigned_to_user_was_cleared ? ' cleared assigned user' : ''}
+
+                {props.data.assigned_group_was_cleared ? ' cleared assigned group' : ''}
+            </div>
         </div>
     )
 }
