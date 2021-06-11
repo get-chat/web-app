df7c08d5fb0cce9d524277ac156a4e7687cb0c5f
berkayakin <siratofficial@gmail.com>
2021-06-11 16:19:17 +0200

    Bulk send update

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1253,6 +1253,7 @@ export default function Chat(props) {
             <TemplateMessages
                 waId={waId}
                 templatesData={props.templates}
+                bulkSendMessage={bulkSendMessage}
                 onSend={(templateMessage) => sendTemplateMessage(templateMessage)}
                 isLoadingTemplates={props.isLoadingTemplates} />
             }
