80ad0628f3c96aa2f79ccfd31c560a4d7ed1c537
berkayakin <siratofficial@gmail.com>
2021-06-29 11:44:10 +0200

    Bulk send indicator update

--- src/components/Sidebar/BulkSendIndicator.js
+++ src/components/Sidebar/BulkSendIndicator.js
@@ -1,10 +1,24 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import '../../styles/BulkSendIndicator.css';
 import {LinearProgress} from "@material-ui/core";
+import PubSub from "pubsub-js";
+import {EVENT_TOPIC_BULK_MESSAGE_TASK_ELEMENT, EVENT_TOPIC_SEND_TEMPLATE_MESSAGE_ERROR} from "../../Constants";
 
 function BulkSendIndicator(props) {
 
     const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const onBulkMessageTaskElement = function (msg, data) {
+            console.log(data);
+        }
+
+        const bulkMessageTaskElementEventToken = PubSub.subscribe(EVENT_TOPIC_BULK_MESSAGE_TASK_ELEMENT, onBulkMessageTaskElement);
+
+        return () => {
+            PubSub.unsubscribe(bulkMessageTaskElementEventToken);
+        }
+    }, []);
 
     return (
         <div className="bulkSendIndicator">
