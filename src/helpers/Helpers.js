2b6ad07d7a14f521aaa270d42676c0ef8b40842e
berkayakin <siratofficial@gmail.com>
2021-06-30 09:49:36 +0200

    Updated URLs with staging server

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -193,8 +193,8 @@ export const getBaseURL = () => {
 export const getWebSocketURL = () => {
     const baseURL = getBaseURL();
     if (baseURL.includes('localhost') || baseURL.includes('10.0.2.2')) {
-        return 'ws://localhost:6789/';
-        //return 'wss://websockets.whatsapp.kondz.io/';
+        //return 'ws://localhost:6789/';
+        return 'wss://websockets.staging.stagingwainbox.360dialog.com/';
     } else {
         return baseURL.replace('https://', 'wss://websockets-').replace('http://', 'wss://websockets-');
     }
