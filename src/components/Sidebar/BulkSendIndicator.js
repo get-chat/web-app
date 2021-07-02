37ba44469b69e875dd3f53a69f341f30fa0cc0f2
berkayakin <siratofficial@gmail.com>
2021-07-02 14:22:43 +0200

    Update

--- src/components/Sidebar/BulkSendIndicator.js
+++ src/components/Sidebar/BulkSendIndicator.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import '../../styles/BulkSendIndicator.css';
 import {LinearProgress} from "@material-ui/core";
 import PubSub from "pubsub-js";
-import {EVENT_TOPIC_BULK_MESSAGE_TASK_ELEMENT, EVENT_TOPIC_SEND_TEMPLATE_MESSAGE_ERROR} from "../../Constants";
+import {EVENT_TOPIC_BULK_MESSAGE_TASK_ELEMENT} from "../../Constants";
 
 function BulkSendIndicator(props) {
 
