b5380d530ca3243c884f8a94a370cb5826da5ed5
berkayakin <siratofficial@gmail.com>
2021-07-14 13:56:42 +0200

    Updated websocket URL generation to handle "/" as base URL

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -191,13 +191,21 @@ export const getBaseURL = () => {
 }
 
 export const getWebSocketURL = () => {
-    return BASE_URL
+    let baseUrlEnv = BASE_URL;
+    if (!baseUrlEnv || baseUrlEnv === "/") {
+        const baseURL = getBaseURL();
+        return prepareWebsocketUrl(baseURL);
+    } else {
+        return prepareWebsocketUrl(baseUrlEnv);
+    }
+}
+
+const prepareWebsocketUrl = (url) => {
+    return url
         .replace('https://', 'wss://websockets-')
         .replace('http://', 'wss://websockets-')
         .replace('api/v1/', '')
         .replace('api/v2/', '');
-
-    //return 'ws://localhost:6789/';
 }
 
 export const displaySeconds = (seconds) => {
