2372e7250d5dfff2ecbc5b3505d096f983950e55
berkayakin <siratofficial@gmail.com>
2021-07-23 16:50:23 +0200

    Sanitizing saved responses

--- src/SavedResponseClass.js
+++ src/SavedResponseClass.js
@@ -1,9 +1,19 @@
+import dompurify from "dompurify";
+
 class SavedResponseClass {
 
     constructor(data) {
         this.id = data.id;
         this.text = data.text;
         this.timestamp = data.timestamp;
+    }
+
+    purify() {
+        const sanitizer = dompurify.sanitize;
+
+        if (this.text) {
+            this.text = sanitizer(this.text);
+        }
     }
 }
 
