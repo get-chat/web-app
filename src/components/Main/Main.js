5bcef28b61c50d49a0d1749879a9c42d2e494965
berkayakin <siratofficial@gmail.com>
2021-09-14 10:55:34 +0200

    Optimized imports

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,7 +9,6 @@ import SearchMessage from "../SearchMessage";
 import ContactDetails from "./ContactDetails";
 import LoadingScreen from "./LoadingScreen";
 import TemplateMessageClass from "../../TemplateMessageClass";
-import * as Sentry from "@sentry/react";
 import {Alert} from "@material-ui/lab";
 import {
     EVENT_TOPIC_BULK_MESSAGE_TASK,
