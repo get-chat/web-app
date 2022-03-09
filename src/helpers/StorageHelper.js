61ce9ca1477b5f72086f403b825b3eb0f92f9135
Berkay AKIN <berkay@get.chat>
2022-03-09 14:55:59 +0100

    Improved StorageHelper.js by using the alias method

--- src/helpers/StorageHelper.js
+++ src/helpers/StorageHelper.js
@@ -27,52 +27,52 @@ export const getStorage = () => {
 }
 
 export const getToken = () => {
-    return getActiveStorage().getItem(STORAGE_TAG_TOKEN);
+    return getStorage().getItem(STORAGE_TAG_TOKEN);
 }
 
 export const storeToken = (token) => {
-    getActiveStorage().setItem(STORAGE_TAG_TOKEN, token);
+    getStorage().setItem(STORAGE_TAG_TOKEN, token);
 }
 
 export const clearToken = () => {
-    getActiveStorage().removeItem(STORAGE_TAG_TOKEN);
+    getStorage().removeItem(STORAGE_TAG_TOKEN);
 }
 
 export const getDisplayAssignmentAndTaggingHistory = () => {
-    const result = getActiveStorage().getItem(STORAGE_TAG_DISPLAY_ASSIGNMENT_AND_TAGGING_HISTORY);
+    const result = getStorage().getItem(STORAGE_TAG_DISPLAY_ASSIGNMENT_AND_TAGGING_HISTORY);
     return result ? result === 'true' : true;
 }
 
 export const setDisplayAssignmentAndTaggingHistory = (willDisplay) => {
-    return getActiveStorage().setItem(STORAGE_TAG_DISPLAY_ASSIGNMENT_AND_TAGGING_HISTORY, willDisplay);
+    return getStorage().setItem(STORAGE_TAG_DISPLAY_ASSIGNMENT_AND_TAGGING_HISTORY, willDisplay);
 }
 
 export const getContactProvidersData = () => {
     clearContactProvidersDataIfExpired();
 
-    const data = getActiveStorage().getItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA);
+    const data = getStorage().getItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA);
     return data ? JSON.parse(data) ?? {} : {};
 }
 
 export const storeContactProvidersData = (data) => {
-    getActiveStorage().setItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA, JSON.stringify(data));
+    getStorage().setItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA, JSON.stringify(data));
 }
 
 export const clearContactProvidersData = () => {
-    getActiveStorage().removeItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA);
+    getStorage().removeItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA);
 }
 
 export const getContactProvidersDataTime = () => {
-    const data = getActiveStorage().getItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME);
+    const data = getStorage().getItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME);
     return data ? JSON.parse(data) : undefined;
 }
 
 export const storeContactProvidersDataTime = (time) => {
-    getActiveStorage().setItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME, time);
+    getStorage().setItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME, time);
 }
 
 export const clearContactProvidersDataTime = () => {
-    getActiveStorage().removeItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME);
+    getStorage().removeItem(STORAGE_TAG_CONTACT_PROVIDERS_DATA_TIME);
 }
 
 const clearContactProvidersDataIfExpired = () => {
