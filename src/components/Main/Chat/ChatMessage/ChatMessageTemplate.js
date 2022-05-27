371d80673eecf72ab85d411145ac005a29ca24c4
Berkay AKIN <berkay@get.chat>
2022-05-27 11:14:43 +0200

    Removed unused variable

--- src/components/Main/Chat/ChatMessage/ChatMessageTemplate.js
+++ src/components/Main/Chat/ChatMessage/ChatMessageTemplate.js
@@ -9,7 +9,7 @@ import {useTranslation} from "react-i18next";
 
 function ChatMessageTemplate(props) {
 
-    const { t, i18n } = useTranslation();
+    const { t } = useTranslation();
 
     const data = props.data;
     const templateData = props.templateData;
