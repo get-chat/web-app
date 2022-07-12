e78cd52ccc8c985e3417d641f32215783b4ef8c7
Berkay AKIN <berkay@get.chat>
2022-07-12 12:00:22 +0200

    Implemented react-linkify to linkify links in Text component

--- src/components/PrintMessage/components/Text/Text.js
+++ src/components/PrintMessage/components/Text/Text.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Linkify from 'react-linkify';
 
-const Text = ({ data: { text } }) => text;
+const Text = ({ data: { text } }) => {
+	return <Linkify componentDecorator={(decoratedHref, decoratedText, key) => (
+		<a target="blank" href={decoratedHref} key={key}>
+			{decoratedText}
+		</a>)}>{text}</Linkify>
+};
 
 export default Text;
