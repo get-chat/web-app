9373974650512e6348c394bea5e3a044ea6539e8
Berkay AKIN <berkay@get.chat>
2022-07-12 13:22:24 +0200

    Removed linkify method

--- src/helpers/MessageHelper.js
+++ src/helpers/MessageHelper.js
@@ -1,41 +1,12 @@
-import { replaceEmojis } from './EmojiHelper';
-import { getLastObject } from './ObjectHelper';
+import {replaceEmojis} from './EmojiHelper';
+import {getLastObject} from './ObjectHelper';
 
-import { sanitize } from 'dompurify';
-
-export function linkify(inputText) {
-	let replacedText, replacePattern1, replacePattern2, replacePattern3;
-
-	// URLs starting with http://, https://, or ftp://
-	replacePattern1 =
-		/(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim;
-	replacedText = inputText.replace(
-		replacePattern1,
-		'<a href="$1" target="_blank">$1</a>'
-	);
-
-	// URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-	replacePattern2 = /(^|[^/])(www\.[\S]+(\b|$))/gim;
-	replacedText = replacedText.replace(
-		replacePattern2,
-		'$1<a href="http://$2" target="_blank">$2</a>'
-	);
-
-	// Change email addresses to mailto:: links.
-	replacePattern3 = /(([a-zA-Z0-9\-_.])+@[a-zA-Z_]+?(\.[a-zA-Z]{2,6})+)/gim;
-	replacedText = replacedText.replace(
-		replacePattern3,
-		'<a href="mailto:$1">$1</a>'
-	);
-
-	return replacedText;
-}
+import {sanitize} from 'dompurify';
 
 export const formatMessage = (message) => {
 	if (!message) return;
 
 	let formatted = message.replaceAll('\n', '<br/>');
-	formatted = linkify(formatted);
 	formatted = sanitize(formatted);
 	formatted = replaceEmojis(formatted);
 
