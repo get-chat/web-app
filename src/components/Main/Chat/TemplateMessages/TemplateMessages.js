0dde496ccb267ed2c6d6e7522a55e02e99a7a653
Berkay AKIN <berkay@get.chat>
2022-09-30 17:22:29 +0200

    Displaying template name in templates list

--- src/components/Main/Chat/TemplateMessages/TemplateMessages.js
+++ src/components/Main/Chat/TemplateMessages/TemplateMessages.js
@@ -151,6 +151,7 @@ function TemplateMessages(props) {
 								<div className="chat__message chat__outgoing messageType__template">
 									{/*<span className={"templateMessage__status " + template[1].status}>{template[1].status}</span>*/}
 									<div className="templateMessage__message">
+										<h4>{template[1].name}</h4>
 										{sortTemplateComponents(template[1].components).map(
 											(comp, index) => (
 												<div key={index}>
