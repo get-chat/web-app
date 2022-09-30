08fbc9a1d1a6c9936b143f40a29de6e7f092593e
Berkay AKIN <berkay@get.chat>
2022-09-30 15:00:15 +0200

    Read CSV for bulk send template

--- src/components/BulkSendTemplateViaCSV.js
+++ src/components/BulkSendTemplateViaCSV.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import { Button, Dialog } from '@material-ui/core';
 import DialogActions from '@material-ui/core/DialogActions';
 import { useTranslation } from 'react-i18next';
+import { csvToArray } from '../helpers/CSVHelper';
+import { preparePhoneNumber } from '../helpers/PhoneNumberHelper';
+import FileInput from './FileInput';
 
 const BulkSendTemplateViaCSV = ({ open, setOpen }) => {
 	const { t } = useTranslation();
+
+	const csvFileInput = useRef();
+
+	const handleCSV = (file) => {
+		if (!file) return;
+
+		csvToArray(file[0], (array) => {
+			const finalRows = [];
+
+			// Extract and collect phone numbers
+			array.forEach((row) => {
+				if (row.length > 0) {
+					// Extract only digits
+					const waId = preparePhoneNumber(row[0]);
+					if (waId) {
+						// Replace original phone number with prepared wa id
+						row[0] = waId;
+						// Collect rows if they contain a proper phone number
+						finalRows.push(row);
+					}
+				}
+			});
+
+			console.log(finalRows);
+		});
+	};
 
 	const close = () => {
 		setOpen(false);
@@ -25,6 +54,15 @@ const BulkSendTemplateViaCSV = ({ open, setOpen }) => {
 			<DialogActions>
 				<Button onClick={close} color="secondary">
 					{t('Close')}
+				</Button>
+				<FileInput
+					innerRef={csvFileInput}
+					multiple={false}
+					accept=".csv"
+					handleSelectedFiles={handleCSV}
+				/>
+				<Button color="primary" onClick={() => csvFileInput.current.click()}>
+					{t('Upload CSV')}
 				</Button>
 			</DialogActions>
 		</Dialog>
