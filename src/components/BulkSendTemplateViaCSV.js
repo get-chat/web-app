295966703410c7f8250142acb07a8b26df6c624a
Berkay AKIN <berkay@get.chat>
2022-10-05 11:37:00 +0200

    Added primary key column selector with next button and further improvements

--- src/components/BulkSendTemplateViaCSV.js
+++ src/components/BulkSendTemplateViaCSV.js
@@ -5,8 +5,12 @@ import {
 	Button,
 	Dialog,
 	FormControl,
+	FormControlLabel,
+	FormLabel,
 	InputLabel,
 	MenuItem,
+	Radio,
+	RadioGroup,
 	Select,
 	Step,
 	StepLabel,
@@ -24,6 +28,7 @@ import {
 	getTemplateParams,
 	templateParamToInteger,
 } from '../helpers/TemplateMessageHelper';
+import { isEmptyString } from '../helpers/Helpers';
 
 const BulkSendTemplateViaCSV = ({ open, setOpen, templates }) => {
 	const { t } = useTranslation();
@@ -74,13 +79,18 @@ const BulkSendTemplateViaCSV = ({ open, setOpen, templates }) => {
 			finalData.push(generateTemplateParamsByValues(template, curData));
 		});
 
-		setActiveStep(2);
+		setActiveStep(3);
 
 		console.log(finalData);
 	};
 
 	function getSteps() {
-		return [t('Upload a CSV file'), t('Select a template'), t('Preview')];
+		return [
+			t('Upload a CSV file'),
+			t('Select the primary key column'),
+			t('Select a template'),
+			t('Preview'),
+		];
 	}
 
 	const updateParam = (event, index, paramKey) => {
@@ -90,6 +100,37 @@ const BulkSendTemplateViaCSV = ({ open, setOpen, templates }) => {
 
 			return { ...nextState };
 		});
+	};
+
+	const handleNext = () => {
+		switch (activeStep) {
+			case 0:
+				csvFileInput.current.click();
+				break;
+			default:
+				setActiveStep((prevState) => prevState + 1);
+				return;
+		}
+	};
+
+	const getNextButtonTitle = () => {
+		switch (activeStep) {
+			case 0:
+				return t('Upload CSV');
+			case 3:
+				return t('Send');
+			default:
+				return t('Next');
+		}
+	};
+
+	const getNextButtonEnabled = () => {
+		switch (activeStep) {
+			case 1:
+				return !isEmptyString(primaryKeyColumn);
+			default:
+				return true;
+		}
 	};
 
 	useEffect(() => {
@@ -128,6 +169,30 @@ const BulkSendTemplateViaCSV = ({ open, setOpen, templates }) => {
 					</div>
 				)}
 				{activeStep === 1 && (
+					<div>
+						<FormControl>
+							<FormLabel>
+								{t('Select the primary key column (phone numbers or tags)')}
+							</FormLabel>
+							<RadioGroup
+								aria-label="primary-key"
+								value={primaryKeyColumn}
+								onChange={(event) => setPrimaryKeyColumn(event.target.value)}
+							>
+								{csvHeader
+									?.filter((headerColumn) => !isEmptyString(headerColumn))
+									?.map((headerColumn) => (
+										<FormControlLabel
+											value={headerColumn}
+											control={<Radio />}
+											label={headerColumn}
+										/>
+									))}
+							</RadioGroup>
+						</FormControl>
+					</div>
+				)}
+				{activeStep === 2 && (
 					<div className="bulkSendTemplateViaCSV__templatesOuterWrapper">
 						<div className="bulkSendTemplateViaCSV__templatesWrapper">
 							<TemplatesList
@@ -137,7 +202,7 @@ const BulkSendTemplateViaCSV = ({ open, setOpen, templates }) => {
 						</div>
 					</div>
 				)}
-				{activeStep === 2 && (
+				{activeStep === 3 && (
 					<div>
 						{template?.components.map((comp, compIndex) => (
 							<div key={compIndex}>
@@ -185,8 +250,12 @@ const BulkSendTemplateViaCSV = ({ open, setOpen, templates }) => {
 					accept=".csv"
 					handleSelectedFiles={handleCSV}
 				/>
-				<Button color="primary" onClick={() => csvFileInput.current.click()}>
-					{t('Upload CSV')}
+				<Button
+					color="primary"
+					onClick={handleNext}
+					disabled={!getNextButtonEnabled()}
+				>
+					{getNextButtonTitle()}
 				</Button>
 			</DialogActions>
 		</Dialog>
