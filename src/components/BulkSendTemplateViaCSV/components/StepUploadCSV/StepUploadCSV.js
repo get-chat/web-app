99a96958051c23f27ad24e363a5cf259a476c31b
Berkay AKIN <berkay@get.chat>
2022-10-06 13:53:27 +0200

    Updated upload CSV step text

--- src/components/BulkSendTemplateViaCSV/components/StepUploadCSV/StepUploadCSV.js
+++ src/components/BulkSendTemplateViaCSV/components/StepUploadCSV/StepUploadCSV.js
@@ -4,11 +4,16 @@ import { Alert, AlertTitle } from '@material-ui/lab';
 const StepUploadCSV = ({ t, csvError }) => {
 	return (
 		<div>
-			<div>
+			<p>
 				{t(
-					'You can upload a CSV file that contains a phone number and template parameters in every row.'
+					'Please upload a CSV file containing the data for the message you want to send in bulk.'
 				)}
-			</div>
+			</p>
+			<p className={'mt-3'}>
+				{t(
+					'The CSV file you upload must contain canonical phone numbers or tags representing recipients. In addition, this file may contain parameter data for the template message you will choose.'
+				)}
+			</p>
 			{csvError !== undefined && (
 				<Alert severity="error" className="bulkSendTemplateViaCSV__csvError">
 					<AlertTitle>{t('Error')}</AlertTitle>
