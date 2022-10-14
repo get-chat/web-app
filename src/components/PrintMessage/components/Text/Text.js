6227a5d1830d9bf9d6011c130c067d00e25a399c
Berkay AKIN <berkay@get.chat>
2022-10-14 15:42:25 +0200

    Added match indexes

--- src/components/PrintMessage/components/Text/Text.js
+++ src/components/PrintMessage/components/Text/Text.js
@@ -3,7 +3,9 @@ import reactStringReplace from 'react-string-replace';
 
 const Text = ({ data: { text } }) => {
 	const regex = /\*(.*?)\*/gi;
-	return reactStringReplace(text, regex, (match) => <strong>{match}</strong>);
+	return reactStringReplace(text, regex, (match, index) => (
+		<strong key={index}>{match}</strong>
+	));
 };
 
 export default Text;
