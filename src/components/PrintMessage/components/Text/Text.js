ec0c4cb6e92f99170c76249884efbb46fd726745
Berkay AKIN <berkay@get.chat>
2022-10-14 15:38:41 +0200

    Displaying bold text using react-string-replace

--- src/components/PrintMessage/components/Text/Text.js
+++ src/components/PrintMessage/components/Text/Text.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import reactStringReplace from 'react-string-replace';
 
 const Text = ({ data: { text } }) => {
-	return text;
+	const regex = /\*(.*?)\*/gi;
+	return reactStringReplace(text, regex, (match) => <strong>{match}</strong>);
 };
 
 export default Text;
