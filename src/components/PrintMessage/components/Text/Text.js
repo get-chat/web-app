194fdef378eb987d3cef483f8649ea13c66544fd
Berkay AKIN <berkay@get.chat>
2022-10-19 12:07:03 +0200

    Replaced formatting with react-string-replace-recursively

--- src/components/PrintMessage/components/Text/Text.js
+++ src/components/PrintMessage/components/Text/Text.js
@@ -1,71 +1,41 @@
 import React, { useMemo } from 'react';
-import reactStringReplace from 'react-string-replace';
+import reactStringReplace from 'react-string-replace-recursively';
 
 const Text = ({ data: { text } }) => {
 	return useMemo(() => {
-		const reactStringReplaceRecursive = (input, pattern, fn, key = 0) => {
-			const isEmpty = (item) => {
-				if (!item) return true;
-				if (item.hasOwnProperty('props')) {
-					return false;
-				} else {
-					return !item.length;
-				}
-			};
-
-			if (!input) {
-				return null;
-			} else if (typeof input === 'string') {
-				return reactStringReplace(input, pattern, fn);
-			}
-
-			const output = [];
-			for (let i = 0; i < input.length; i++) {
-				const item = input[i];
-				if (item) {
-					if (typeof item === 'string') {
-						const next = reactStringReplace(item, pattern, fn);
-						if (!isEmpty(next)) output.push(next);
-					} else if (typeof item === 'object') {
-						if (
-							item.hasOwnProperty('props') &&
-							item.props.hasOwnProperty('children')
-						) {
-							const next = reactStringReplaceRecursive(
-								item.props.children,
-								pattern,
-								fn,
-								key + 1
-							);
-							if (!isEmpty(next)) {
-								const props = Object.assign(
-									{ key: 'k' + key + 'i' + i },
-									item.props
-								);
-								output.push(React.createElement(item.type, props, next));
-							}
-						} else {
-							const next = reactStringReplaceRecursive(
-								item,
-								pattern,
-								fn,
-								key + 1
-							);
-							if (!isEmpty(next)) output.push(next);
-						}
-					}
-				}
-			}
-
-			return output;
-		};
-
 		const boldRegex = /\*(.*?)\*/gi;
 		const italicRegex = /_(.*?)_/gi;
 		const strikeRegex = /~(.*?)~/gi;
 		const codeRegex = /```(.*?)```/gi;
 
-		let formattedText = text;
+		const config = {
+			bold: {
+				pattern: boldRegex,
+				matcherFn: function (rawText, processed, key) {
+					return <strong key={key}>{processed}</strong>;
+				},
+			},
+			italic: {
+				pattern: italicRegex,
+				matcherFn: function (rawText, processed, key) {
+					return <i key={key}>{processed}</i>;
+				},
+			},
+			strike: {
+				pattern: strikeRegex,
+				matcherFn: function (rawText, processed, key) {
+					return <s key={key}>{processed}</s>;
+				},
+			},
+			code: {
+				pattern: codeRegex,
+				matcherFn: function (rawText, processed, key) {
+					return <code key={key}>{processed}</code>;
+				},
+			},
+		};
+
+		/*let formattedText = text;
 
 		formattedText = reactStringReplaceRecursive(
 			formattedText,
@@ -89,9 +59,9 @@ const Text = ({ data: { text } }) => {
 			formattedText,
 			codeRegex,
 			(match, index) => <code key={'code_' + index}>{match}</code>
-		);
+		);*/
 
-		return formattedText;
+		return reactStringReplace(config)(text);
 	}, [text]);
 };
 
