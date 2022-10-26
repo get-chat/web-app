0fe86cd4accfda4f0d562c50ab9e77521b29afb4
Berkay AKIN <berkay@get.chat>
2022-10-26 12:39:46 +0200

    Updated zoom feature for mobile

--- src/components/Main/PreviewMediaZoom.js
+++ src/components/Main/PreviewMediaZoom.js
@@ -7,40 +7,61 @@ const PreviewMediaZoom = ({ src, onClick }) => {
 	useEffect(() => {
 		let debounceTimer;
 
+		const move = (currentTarget, currentX, currentY) => {
+			if (currentX < 0) currentX = 0;
+			if (currentX > currentTarget.offsetWidth)
+				currentX = currentTarget.offsetWidth;
+			let targetX = currentTarget.offsetWidth / 2 - currentX;
+			if (currentX > currentTarget.offsetWidth) {
+				targetX = targetX * -1;
+			}
+
+			if (currentY < 0) currentY = 0;
+			if (currentY > currentTarget.offsetHeight)
+				currentY = currentTarget.offsetHeight;
+			let targetY = currentTarget.offsetHeight / 2 - currentY;
+			if (currentY > currentTarget.offsetHeight) {
+				targetY = targetY * -1;
+			}
+
+			zoomView.current.style.transform =
+				'translateX(' + targetX + 'px) translateY(' + targetY + 'px) scale(2)';
+		};
+
 		const handleMouseMove = (e) => {
 			if (debounceTimer) {
 				window.clearTimeout(debounceTimer);
 			}
 
-			let currentTarget = e.currentTarget;
-			let currentX = e.x;
-			let currentY = e.y;
+			let _currentTarget = e.currentTarget;
+			let _currentX = e.x;
+			let _currentY = e.y;
 
 			debounceTimer = setTimeout(function () {
-				let targetX = currentTarget.offsetWidth / 2 - currentX;
-				if (currentX > currentTarget.offsetWidth) {
-					targetX = targetX * -1;
-				}
+				move(_currentTarget, _currentX, _currentY);
+			}, 5);
+		};
 
-				let targetY = currentTarget.offsetHeight / 2 - currentY;
-				if (currentY > currentTarget.offsetHeight) {
-					targetY = targetY * -1;
-				}
+		const handleTouchMove = (e) => {
+			if (debounceTimer) {
+				window.clearTimeout(debounceTimer);
+			}
 
-				zoomView.current.style.transform =
-					'translateX(' +
-					targetX +
-					'px) translateY(' +
-					targetY +
-					'px) scale(2)';
+			let _currentTarget = e.currentTarget;
+			let _currentX = e.changedTouches[0].clientX;
+			let _currentY = e.changedTouches[0].clientY;
+
+			debounceTimer = setTimeout(function () {
+				move(_currentTarget, _currentX, _currentY);
 			}, 5);
 		};
 
 		zoomView.current.addEventListener('mousemove', handleMouseMove);
+		zoomView.current.addEventListener('touchmove', handleTouchMove);
 
 		return () => {
-			console.log(zoomView.current);
 			zoomView.current.removeEventListener('mousemove', handleMouseMove);
+			zoomView.current.removeEventListener('touchmove', handleTouchMove);
 		};
 	}, [zoomView]);
 
