e5c9b89dca599fb97db0ec7e498c19e052f0daa8
Anton Frolovsky <antonfrolovsky@gmail.com>
2022-11-12 05:29:50 +0400

    Add html-template-plugin & dev-server

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -7,6 +9,7 @@ module.exports = {
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, '../build'),
+		clean: true,
 	},
 	resolve: {
 		extensions: ['.js', '.jsx', '.css', '.json'],
@@ -31,5 +34,28 @@ module.exports = {
 				type: 'asset/resource',
 			},
 		],
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.REACT_APP_LOGO_URL': JSON.stringify(
+				process.env.REACT_APP_LOGO_URL
+			),
+			'process.env.REACT_APP_LOGO_BLACK_URL': JSON.stringify(
+				process.env.REACT_APP_LOGO_BLACK_URL
+			),
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Output Management',
+			template: path.resolve(__dirname, '../public/index.html'),
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, '../public'),
+		},
+		historyApiFallback: true,
+		compress: true,
+		port: 9000,
+		open: true,
 	},
 };
