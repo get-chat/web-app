b4ab371a97fa8311950d1c103c619262b36607e3
Berkay AKIN <berkay@get.chat>
2022-11-22 14:04:08 +0100

    Improved finding tag

--- src/components/ChatTag/ChatTag.js
+++ src/components/ChatTag/ChatTag.js
@@ -1,23 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import LabelIcon from '@material-ui/icons/Label';
 import { useSelector } from 'react-redux';
 
 const ChatTag = ({ id }) => {
 	const tags = useSelector((state) => state.tags.value);
 
-	const [tag, setTag] = useState();
-
-	useEffect(() => {
-		if (tags) {
-			for (let i = 0; i < tags.length; i++) {
-				const curTag = tags[i];
-				if (curTag.id === id) {
-					setTag(curTag);
-					break;
-				}
-			}
+	const tag = useMemo(() => {
+		if (tags && id) {
+			return tags.find((tag) => tag.id === id);
 		}
-	}, [id, tags]);
+	}, [tags, id]);
 
 	return (
 		<LabelIcon
