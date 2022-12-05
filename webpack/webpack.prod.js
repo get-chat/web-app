e197fca6fa9e2ad2569b351084b6c033ed2cfca2
Anton Frolovsky <antonfrolovsky@gmail.com>
2022-12-05 18:59:25 +0400

    copy public folder into build

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,22 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
 const commonWebpackConfig = require('./webpack.common.js');
 
 module.exports = merge(commonWebpackConfig, {
 	mode: 'production',
 	stats: 'normal',
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, '../public'),
+					to: path.resolve(__dirname, '../build'),
+					globOptions: {
+						ignore: ['**/index.html'],
+					},
+				},
+			],
+		}),
+	],
 });
