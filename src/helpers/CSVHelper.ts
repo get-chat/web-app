070597dd55f1339df3f9ad86223fb27ebb87f3a2
Berkay AKIN <berkay@get.chat>
2023-03-20 14:32:43 +0100

    Added types

--- src/helpers/CSVHelper.ts
+++ src/helpers/CSVHelper.ts
@@ -1,27 +1,37 @@
-// @ts-nocheck
-export const csvToObj = (file, callback) => {
+export const csvToObj = (
+	file: File,
+	callback: ({}: { header: string[]; data: { [key: string]: string }[] }) => any
+) => {
 	const fileReader = new FileReader();
 
 	fileReader.onload = function (event) {
-		const string = event.target.result;
+		const string = event.target?.result?.toString();
 
-		const csvHeader = string.slice(0, string.indexOf('\n')).split(',');
-		const csvRows = string.slice(string.indexOf('\n') + 1).split('\n');
+		console.log(string);
 
-		//const rows = csvRows.map((i) => i.split(','));
+		if (string) {
+			const csvHeader = string.slice(0, string.indexOf('\n')).split(',');
+			const csvRows = string.slice(string.indexOf('\n') + 1).split('\n');
 
-		const rows = csvRows.filter(Boolean).map((i) => {
-			const values = i.split(',');
-			return csvHeader.reduce((object, header, index) => {
-				object[header] = values[index];
-				return object;
-			}, {});
-		});
+			const rows = csvRows.filter(Boolean).map((i) => {
+				const values = i.split(',');
+				return csvHeader.reduce(
+					(object: { [key: string]: string }, header, index) => {
+						object[header] = values[index];
+						return object;
+					},
+					{}
+				);
+			});
 
-		callback?.({
-			header: csvHeader,
-			data: rows,
-		});
+			console.log(csvHeader);
+			console.log(rows);
+
+			callback?.({
+				header: csvHeader,
+				data: rows,
+			});
+		}
 	};
 
 	fileReader.readAsText(file);
