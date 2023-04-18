b0b6b286a6b08917765cf2d606e1d225f8fdc4f4
Berkay AKIN <berkay@get.chat>
2023-04-18 11:31:39 +0200

    Quick actions initial

---
+++ src/components/QuickActionsMenu/QuickActionsMenu.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export type Props = {
+	input: string;
+	setInput: (text?: string) => any;
+	onProcessCommand: (text?: string) => any;
+	isExpired: boolean;
+};
+
+const QuickActionsMenu: React.FC<Props> = ({
+	input,
+	setInput,
+	onProcessCommand,
+	isExpired,
+}) => {
+	return <div>Quick actions menu</div>;
+};
+
+export default QuickActionsMenu;
