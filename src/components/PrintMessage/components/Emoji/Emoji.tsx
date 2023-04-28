a669b10d2f10aa9aa57f940465d18a5ecb96b815
Berkay AKIN <berkay@get.chat>
2023-04-28 14:24:46 +0200

    Added print message component props

--- src/components/PrintMessage/components/Emoji/Emoji.tsx
+++ src/components/PrintMessage/components/Emoji/Emoji.tsx
@@ -1,9 +1,11 @@
-// @ts-nocheck
 import React from 'react';
 
 import { replaceEmojis } from '@src/helpers/EmojiHelper';
+import { PrintMessageComponentProps } from '@src/components/PrintMessage/components/PrintMessageComponentProps';
 
-const Emoji = ({ data: { text, single } }) => {
+const Emoji: React.FC<PrintMessageComponentProps> = ({
+	data: { text, single },
+}) => {
 	return (
 		<span dangerouslySetInnerHTML={{ __html: replaceEmojis(text, !single) }} />
 	);
