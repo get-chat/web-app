71291f5ed15689cfe95c23c892f16c8e4af4f8d8
Berkay AKIN <berkay@get.chat>
2023-05-01 16:15:09 +0200

    TypeScript update for PersonModel

--- src/api/models/PersonModel.ts
+++ src/api/models/PersonModel.ts
@@ -1,10 +1,15 @@
-// @ts-nocheck
 import { getPastHoursByTimestamp } from '@src/helpers/DateHelper';
 import { generateInitialsHelper } from '@src/helpers/Helpers';
 import { parseIntSafely } from '@src/helpers/IntegerHelper';
 
 class PersonModel {
-	constructor(data) {
+	public waId?: string;
+	public name?: string;
+	public initials?: string;
+	public lastMessageTimestamp: Number = -1;
+	public isExpired = false;
+
+	constructor(data: any) {
 		const payload = data.waba_payload;
 
 		this.waId = data.wa_id;
@@ -16,7 +21,7 @@ class PersonModel {
 		this.isExpired = this.checkIfExpired();
 	}
 
-	setName(name) {
+	setName(name: string) {
 		this.name = name;
 		this.initials = this.generateInitials(); //data.initials;
 	}
