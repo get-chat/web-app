57e52019876771a78102481515415ab474701bf7
Berkay AKIN <berkay@get.chat>
2023-05-01 12:04:39 +0200

    Improved custom avatar usage

--- src/components/CustomAvatar/CustomAvatar.tsx
+++ src/components/CustomAvatar/CustomAvatar.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Avatar } from '@mui/material';
 
 import styles from './CustomAvatar.module.css';
 import classNames from 'classnames/bind';
+import { generateAvatarColor } from '@src/helpers/AvatarHelper';
 
 const cx = classNames.bind(styles);
 
@@ -10,6 +11,7 @@ interface Props {
 	className?: string;
 	src?: string;
 	style?: any;
+	generateBgColorBy?: string;
 	alt?: string;
 	ref?: React.MutableRefObject<any>;
 	onClick?: () => void;
@@ -20,19 +22,29 @@ const CustomAvatar: React.FC<Props> = ({
 	className,
 	src,
 	style,
+	generateBgColorBy,
 	alt,
 	ref,
 	onClick,
 	children,
 }) => {
+	const bgColor = useMemo(() => {
+		return generateBgColorBy
+			? generateAvatarColor(generateBgColorBy)
+			: undefined;
+	}, [generateBgColorBy]);
+
 	return (
 		<Avatar
 			className={cx({
 				[className ?? '']: true,
-				light: !Boolean(style?.backgroundColor),
+				light: !Boolean(style?.backgroundColor ?? bgColor),
 			})}
 			src={src}
-			style={style}
+			style={{
+				...style,
+				backgroundColor: bgColor ?? style?.backgroundColor,
+			}}
 			alt={alt}
 			onClick={onClick}
 			ref={ref}
