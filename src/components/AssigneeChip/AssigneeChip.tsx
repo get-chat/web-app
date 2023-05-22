eed11922cb8a02cd9c4b7f9a04a31f1b7fc080d1
Berkay AKIN <berkay@get.chat>
2023-05-22 14:07:16 +0200

    Displaying user options

--- src/components/AssigneeChip/AssigneeChip.tsx
+++ src/components/AssigneeChip/AssigneeChip.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEvent, useState } from 'react';
 import CustomAvatar from '@src/components/CustomAvatar';
 import { generateInitialsHelper } from '@src/helpers/Helpers';
 import styles from './AssigneeChip.module.css';
@@ -6,8 +6,10 @@ import GroupIcon from '@mui/icons-material/Group';
 import PersonIcon from '@mui/icons-material/Person';
 import classNames from 'classnames/bind';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { IconButton } from '@mui/material';
+import { IconButton, Menu, MenuItem } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { useAppSelector } from '@src/store/hooks';
+import { UserList } from '@src/api/responses/UsersResponse';
 
 enum AssigneeType {
 	user = 'user',
@@ -28,6 +30,24 @@ const AssigneeChip: React.FC<Props> = ({
 	isActionable = false,
 }) => {
 	const { t } = useTranslation();
+
+	let users: UserList = {};
+
+	if (isActionable) {
+		users = useAppSelector((state) => state.users.value);
+	}
+
+	const [anchorEl, setAnchorEl] = useState<Element>();
+
+	const displayMenu = (event: MouseEvent) => {
+		if (event.currentTarget instanceof Element) {
+			setAnchorEl(event.currentTarget);
+		}
+	};
+
+	const hideMenu = () => {
+		setAnchorEl(undefined);
+	};
 
 	return (
 		<div
@@ -55,13 +75,34 @@ const AssigneeChip: React.FC<Props> = ({
 			<span className={styles.name}>{name ?? t('Unassigned')}</span>
 
 			{isActionable && (
-				<IconButton
-					className={styles.actionIcon}
-					onClick={console.log}
-					size="small"
-				>
-					<ExpandMoreIcon />
-				</IconButton>
+				<>
+					<IconButton
+						className={styles.actionIcon}
+						onClick={displayMenu}
+						size="small"
+					>
+						<ExpandMoreIcon />
+					</IconButton>
+
+					{assigneeType === AssigneeType.user && (
+						<Menu
+							anchorEl={anchorEl}
+							anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+							transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+							keepMounted
+							open={Boolean(anchorEl)}
+							onClose={hideMenu}
+							elevation={3}
+						>
+							{Object.values(users)?.map((user) => (
+								// @ts-ignore
+								<MenuItem key={user.id} value={user.id}>
+									{user.prepareUserLabel()}
+								</MenuItem>
+							))}
+						</Menu>
+					)}
+				</>
 			)}
 		</div>
 	);
