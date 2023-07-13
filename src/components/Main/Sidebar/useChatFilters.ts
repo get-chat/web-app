0aab536b87b60c0d06e821322c63521b741a1128
Berkay AKIN <berkay@get.chat>
2023-07-13 11:36:06 +0200

    Updating query params

--- src/components/Main/Sidebar/useChatFilters.ts
+++ src/components/Main/Sidebar/useChatFilters.ts
@@ -1,7 +1,12 @@
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { setUserPreference } from '@src/helpers/StorageHelper';
 import { useAppDispatch, useAppSelector } from '@src/store/hooks';
-import { useSearchParams } from 'react-router-dom';
+import {
+	createSearchParams,
+	useLocation,
+	useNavigate,
+	useSearchParams,
+} from 'react-router-dom';
 import FilterQueryParams from '@src/enums/FilterQueryParams';
 import { setFilterTagId } from '@src/store/reducers/filterTagIdReducer';
 
@@ -66,6 +71,8 @@ const useChatFilters = () => {
 
 	const isMounted = useRef(false);
 
+	const navigate = useNavigate();
+
 	useEffect(() => {
 		// Here chat filter id in store will be filled if no filter query param is provided
 		if (currentUser && !hasAnyFilterQueryParam) {
@@ -120,6 +127,55 @@ const useChatFilters = () => {
 			isMounted.current = true;
 		}
 	}, [
+		filterAssignedToMe,
+		filterAssignedGroupId,
+		filterTagId,
+		filterStartDate,
+		filterEndDate,
+	]);
+
+	const location = useLocation();
+
+	useEffect(() => {
+		if (isMounted.current) {
+			const params: { [key: string]: any } = {
+				[FilterQueryParams.LIMIT]: chatsLimit,
+				[FilterQueryParams.OFFSET]: chatsOffset,
+				[FilterQueryParams.SEARCH]: keyword,
+				[FilterQueryParams.ASSIGNED_TO_ME]: filterAssignedToMe ? 1 : null,
+				[FilterQueryParams.ASSIGNED_GROUP]: filterAssignedGroupId,
+				[FilterQueryParams.CHAT_TAG_ID]: filterTagId,
+				[FilterQueryParams.MESSAGES_SINCE_TIME]: filterStartDate
+					?.toISOString()
+					?.split('T')[0],
+				[FilterQueryParams.MESSAGES_BEFORE_TIME]: filterEndDate
+					?.toString()
+					?.split('T')[0],
+			};
+
+			// Filter object
+			for (let key in params) {
+				// Delete if empty or limit has default value
+				if (
+					(key === FilterQueryParams.LIMIT && params[key] === LIMIT_DEFAULT) ||
+					!params[key]
+				)
+					delete params[key];
+			}
+
+			// Prepare navigate options
+			const options = {
+				pathname: location.pathname,
+				search: `?${createSearchParams(params)}`,
+			};
+
+			// Update query params
+			navigate(options, { replace: true });
+		}
+	}, [
+		chatsLimit,
+		chatsOffset,
+		keyword,
 		filterAssignedToMe,
 		filterAssignedGroupId,
 		filterTagId,
