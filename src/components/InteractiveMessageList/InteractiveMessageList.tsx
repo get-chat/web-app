ffc91659587452aa426c4adb424d7f6fb4d75130
Berkay AKIN <berkay@get.chat>
2024-10-14 14:54:19 +0200

    Displaying interactive messages

--- src/components/InteractiveMessageList/InteractiveMessageList.tsx
+++ src/components/InteractiveMessageList/InteractiveMessageList.tsx
@@ -1,9 +1,57 @@
 import styles from './InteractiveMessageList.module.css';
+import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const INTERACTIVE_MESSAGES = [
+	{
+		type: 'location_request_message',
+		body: {
+			text: '',
+		},
+		action: {
+			name: 'send_location',
+		},
+	},
+];
 
 const InteractiveMessageList = () => {
+	const { t } = useTranslation();
+	const [text, setText] = useState('');
+
+	const send = (payload: any) => {
+		payload.body.text = text;
+		console.log(payload);
+	};
+
 	return (
 		<div className="interactiveMessagesOuter">
-			<div className="interactiveMessagesWrapper">List</div>
+			<div className="interactiveMessagesWrapper">
+				<div className={styles.textFieldWrapper}>
+					<TextField
+						variant="filled"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+						label={t('Enter your message here')}
+						size="medium"
+						multiline={true}
+						fullWidth={true}
+					/>
+				</div>
+
+				{INTERACTIVE_MESSAGES.map((item) => (
+					<div className={styles.item}>
+						<div>{item.type}</div>
+						<Button
+							onClick={() => send(item)}
+							// @ts-ignore
+							color="black"
+						>
+							{t('Send')}
+						</Button>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
