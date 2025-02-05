00e786d689a894b1d17f4afc571b5c87476b01da
Berkay AKIN <berkay@get.chat>
2025-02-05 14:18:03 +0100

    Updated Sentry config

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 
 import { isLocalHost } from '@src/helpers/URLHelper';
 import { isEmptyString } from '@src/helpers/Helpers';
@@ -16,9 +15,18 @@ export const initializeSentry = (config: AppConfig) => {
 			dsn: config.APP_SENTRY_DSN,
 			release: packageJson.version,
 			integrations: [
-				new Integrations.BrowserTracing({ tracePropagationTargets: ['*'] }),
+				Sentry.browserTracingIntegration(),
+				Sentry.replayIntegration(),
 			],
+
+			// Tracing
 			tracesSampleRate: 0.002,
+			// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+			tracePropagationTargets: ['*'],
+			// Session Replay
+			replaysSessionSampleRate: 0.002, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+			replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
 			ignoreErrors: ['ResizeObserver loop limit exceeded'],
 			beforeSend(event) {
 				// Check if it is an exception, and if so, show the report dialog
