ec41721dde9c0ac04aa95b75a8553922ad253d31
Berkay AKIN <berkay@get.chat>
2025-05-06 12:30:18 +0200

    Refactored ContactsModal with Styled Components

--- src/components/ContactsModal/ContactsModal.tsx
+++ src/components/ContactsModal/ContactsModal.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
-import styles from './ContactsModal.module.css';
 import { Trans, useTranslation } from 'react-i18next';
 import { getHubURL } from '@src/helpers/URLHelper';
 import { AppConfigContext } from '@src/contexts/AppConfigContext';
@@ -12,21 +9,7 @@ import Contacts from '@src/components/Contacts';
 import { AxiosResponse } from 'axios';
 import { MessageType } from '@src/types/messages';
 import { Recipient } from '@src/types/persons';
-
-interface DialogHeaderProps {
-	children?: JSX.Element | string;
-	onClose: () => void;
-}
-
-const DialogHeader: React.FC<DialogHeaderProps> = ({
-	children,
-	onClose,
-	...rest
-}) => (
-	<DialogTitle className={styles.header} {...rest}>
-		{children}
-	</DialogTitle>
-);
+import * as Styled from './ContactsModal.styles';
 
 interface Props {
 	open: boolean;
@@ -88,28 +71,24 @@ const ContactsModal: React.FC<Props> = ({
 
 	return (
 		<Dialog open={open} onClose={handleClose} fullWidth>
-			<DialogHeader onClose={handleClose}>{t('Send contacts')}</DialogHeader>
-			<DialogContent classes={{ root: styles.content }} className={'noPadding'}>
+			<Styled.StyledDialogTitle>{t('Send contacts')}</Styled.StyledDialogTitle>
+			<Styled.StyledDialogContent>
 				<Contacts
 					isSelectionModeEnabled={true}
 					selectedContacts={selectedContacts}
 					onSelect={handleSelect}
 					noContactsContent={
-						<div
-							style={{
-								padding: '10px 15px',
-							}}
-						>
+						<Styled.NoContactsContent>
 							<Trans>
 								To be able to share contacts, you need to use one of our Contact
 								Providers.{' '}
 								<a href={getHubURL(config?.API_BASE_URL ?? '')}>Click here</a>{' '}
 								to go to our integrations page.
 							</Trans>
-						</div>
+						</Styled.NoContactsContent>
 					}
 				/>
-			</DialogContent>
+			</Styled.StyledDialogContent>
 			{selectedContacts.length > 0 && (
 				<DialogActions>
 					<Button onClick={handleSendMessage}>{t('Send')}</Button>
