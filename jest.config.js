18a6e822d2b4a9671acf57e06fdfe4a80ce32a44
Berkay AKIN <berkay@get.chat>
2025-06-24 13:23:04 +0200

    Fixed tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 module.exports = {
 	testEnvironment: 'jsdom',
-	moduleFileExtensions: ['js', 'jsx'],
-	moduleDirectories: ['node_modules'],
+	moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 	moduleNameMapper: {
 		'^@src/(.*)$': '<rootDir>/src/$1',
-		'\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-			'<rootDir>/__mocks__/fileMock.js',
-		'\\./recorder/worker': '<rootDir>/__mocks__/workerMock.js',
-		'\\.css$': 'identity-obj-proxy',
+		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+		'\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
 	},
 	transform: {
-		'\\.jsx?$': 'babel-jest',
+		'^.+\\.(js|jsx)$': 'babel-jest',
+		'^.+\\.(ts|tsx)$': 'ts-jest',
 	},
+	testMatch: [
+		'**/*.(test|spec).(js|jsx|ts|tsx)'
+	],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
