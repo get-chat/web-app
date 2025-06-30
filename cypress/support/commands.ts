39afe4d8982fded163d9fe2124cd9e83c3367309
Berkay AKIN <berkay@get.chat>
2025-06-30 15:09:10 +0200

    Added missing Cypress files

---
+++ cypress/support/commands.ts
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+/* eslint-disable no-undef */
+import "@testing-library/cypress/add-commands";
+
+Cypress.Commands.add("login", (username: string, password: string) => {
+	cy.visit("/");
+	cy.get('[data-testid="username"] input').focus().type(username);
+	cy.get('[data-testid="password"] input').focus().type(password);
+	cy.get('[data-testid="submit"]').click();
+	cy.wait(10000);
+	cy.url().should("contain", "/main");
+});
