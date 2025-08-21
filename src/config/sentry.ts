28f17b1e953a61576ab39a358fe1676a61e1e0f0
Berkay AKIN <berkay@get.chat>
2025-08-21 12:16:40 +0200

    Using 360dialog url for trace propagation target

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -9,7 +9,7 @@ import i18next from 'i18next';
 import { getBaseDomain } from '@src/helpers/URLHelper';
 
 export const initializeSentry = (config: AppConfig) => {
-	if (process.env.NODE_ENV === 'production' && config.APP_SENTRY_DSN) {
+	if (config.APP_SENTRY_DSN) {
 		Sentry.init({
 			debug: true,
 			dsn: config.APP_SENTRY_DSN,
@@ -22,10 +22,7 @@ export const initializeSentry = (config: AppConfig) => {
 			// Tracing
 			tracesSampleRate: 0.5,
 			// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-			tracePropagationTargets: [
-				'localhost:3000',
-				getBaseDomain(config.API_BASE_URL),
-			],
+			tracePropagationTargets: ['localhost:3000', 'getchat.360dialog.io'],
 			// Session Replay
 			replaysSessionSampleRate: 0.002, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 			replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
