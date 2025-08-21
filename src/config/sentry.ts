e0c86dfb9b49488c63cfd5e8528a59f28f0fb787
Berkay AKIN <berkay@get.chat>
2025-08-21 10:31:29 +0200

    Generating trace propagation target from api url

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -6,6 +6,7 @@ import packageJson from '../../package.json';
 
 import type { AppConfig } from './application';
 import i18next from 'i18next';
+import { getBaseDomain } from '@src/helpers/URLHelper';
 
 export const initializeSentry = (config: AppConfig) => {
 	if (process.env.NODE_ENV === 'production' && config.APP_SENTRY_DSN) {
@@ -21,7 +22,10 @@ export const initializeSentry = (config: AppConfig) => {
 			// Tracing
 			tracesSampleRate: 0.5,
 			// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-			tracePropagationTargets: ['*'],
+			tracePropagationTargets: [
+				'localhost:3000',
+				getBaseDomain(config.API_BASE_URL),
+			],
 			// Session Replay
 			replaysSessionSampleRate: 0.002, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 			replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
